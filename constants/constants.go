889aec743e392195069a0dcf988b5b697872fe64
dw642 <dw642@cornell.edu>
2025-04-06 15:10:36 -0400

    first commit

---
+++ constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+    USER_INDEX = "user"
+    POST_INDEX = "post"
+    ES_URL = "http://34.86.118.96:9200"
+    ES_USERNAME = "David"
+    ES_PASSWORD = "2002Atlv!"
+	GCS_BUCKET = "david_socialai_20240209"
+)
